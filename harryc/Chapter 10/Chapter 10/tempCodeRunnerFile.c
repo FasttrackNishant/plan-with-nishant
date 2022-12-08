b5b6c2924409c6935e9fb902bd16863c3fea7659
Nishant <ncp1435@gmail.com>
2022-12-08 19:09:31 +0530

    new professional installed

--- harryc/Chapter 10/Chapter 10/tempCodeRunnerFile.c
+++
@@ -1,7 +0,0 @@
- while (c!=EOF)
-
-    // {
-      
-    //    putc(c,ptr2);
-    //    c =fgetc(ptr);
-    // }
--- harryc/Chapter 3/03_practice_set.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main () 
- { 
-int 
-;
-printf(
-);
-return 0;}
